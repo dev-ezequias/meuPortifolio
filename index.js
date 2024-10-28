883e74f846342446afc063642cb0070e87cf0ba2
dev-ezequias <ezequias.sccontato@icloud.com>
2024-10-28 16:41:22 -0300

    docs: terceiro commit

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
-let buttons = document.querySelectorAll("#leia-mais");
+let buttons = document.querySelectorAll(".leia-mais");
 
 buttons.forEach(button => {
-    button.addEventListener("click", function(){
+    button.addEventListener("click", function() {
         let card = this.closest(".section-card");
-        card.classList.toggle("active");
-        if (card.classList.contains("active")){
-            return this.textContent = "Ler menos";
+
+        // Verifica o estado atual do card clicado
+        let isActive = card.classList.contains('active');
+
+        // Fecha todos os cards
+        document.querySelectorAll('.section-card').forEach(c => {
+            c.classList.remove('active');
+            c.querySelector('.content').style.maxHeight = '215px';
+            c.querySelector('.leia-mais').textContent = 'Leia mais';
+        });
+
+        // Se o card clicado não estava ativo, abre ele
+        if (!isActive) {
+            card.classList.add("active");
+            card.querySelector('.content').style.maxHeight = '1000px'; // ou o valor que deseja expandir
+            this.textContent = "Ler menos";
         }
-        this.textContent = "Leia mais";
     });
 });
+
+document.getElementById("limpar").addEventListener("click", function(event){
+    event.preventDefault();
+    document.getElementById("meu-formulario").reset();
+})
